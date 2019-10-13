5910bdca74d304ff1bba0d8c70ae04b602f1e34c
Eldridge Gomes <gomes.eldridge@gmail.com>
2019-10-13 10:46:52 +0530

    Mutations

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect('<connectionString>')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
